bf64ea0e0ecdb8a6360b23b9842308c369607816
trusempai101 <troy.laubach@warriorlife.net>
2018-05-15 10:26:42 -0700

    Added Website

---
+++ website/scripts/4.8.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+function look() {
+	 alert("Hey Stop!");
+	}
+	
+var cookie_score = 0;
+function cookie_clicker() {
+	cookie_score = cookie_score + 1;
+	document.getElementById("score").value = cookie_score;
+}
---
+++ website/scripts/greeting.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+function greeting( ) {
+		alert("I am terible because it is MONDAY!");
+	}
